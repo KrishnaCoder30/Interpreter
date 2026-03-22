3f2652c3940d8c4d1c1144b5c8c63014effd2fef
Krishna Ishwar Deshmukh <krishna@Krishnas-MacBook-Air-805.local>
2026-03-22 18:38:22 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,94 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
-#include<iostream>
 using namespace std;
 
-std::string read_file_contents(const std::string& filename);
+std::string read_file_contents(const std::string &filename);
 
 int main(int argc, char *argv[]) {
-    // Disable output buffering
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
+  // Disable output buffering
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here!" << std::endl;
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
+  std::cerr << "Logs from your program will appear here!" << std::endl;
 
-    if (argc < 3) {
-        std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
-        return 1;
+  if (argc < 3) {
+    std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
+    return 1;
+  }
+
+  const std::string command = argv[1];
+  map<char, string> token = {
+      {'{', "LEFT_BRACE"},  {'}', "RIGHT_BRACE"}, {'(', "LEFT_PAREN"},
+      {')', "RIGHT_PAREN"}, {'*', "STAR"},        {'.', "DOT"},
+      {'+', "PLUS"},        {',', "COMMA"},       {'-', "MINUS"},
+      {';', "SEMICOLON"},
+  };
+  if (command == "tokenize") {
+    std::string file_contents = read_file_contents(argv[2]);
+
+    // TODO: Uncomment the code below to pass the first stage
+    //
+    cerr << "$ ./your_program.sh tokenize test.lox" << endl;
+    for (auto u : file_contents) {
+      // if(u == '{'){
+      //     cout<<"LEFT_BRACE { null"<<endl;
+      // }
+      // else if(u == '}'){
+      //     cout<<"RIGHT_BRACE } null"<<endl;
+      // }
+      // else if(u == '('){
+      //     cout<<"LEFT_PAREN ( null"<<endl;
+      // }
+      // else if(u == ')'){
+      //     cout<<"RIGHT_PAREN ) null"<<endl;
+      // }
+      // else if(u == '*'){
+      //     cout<<"STAR * null"<<endl;
+      // }
+      // else if(u == '.'){
+      //     cout<<"DOT . null"<<endl;
+      // }
+      // else if(u == '+'){
+      //     cout<<"PLUS + null"<<endl;
+      // }
+      // else if(u == ','){
+      //     cout<<"COMMA , null"<<endl;
+      // }
+      // else if(u == '-'){
+      //     cout<<"MINUS - null"<<endl;
+      // }
+      // else if(u == ';'){
+      //     cout<<"SEMICOLON ; null"<<endl;
+      // }
+      cout << token[u] << " " << u << " null" << endl;
     }
+    std::cout << "EOF  null" << std::endl; // Placeholder, replace this line
+                                           // when implementing the scanner
 
-    const std::string command = argv[1];
+  } else {
+    std::cerr << "Unknown command: " << command << std::endl;
+    return 1;
+  }
 
-    if (command == "tokenize") {
-        std::string file_contents = read_file_contents(argv[2]);
-        
-        // TODO: Uncomment the code below to pass the first stage
-        // 
-        cerr<<"$ ./your_program.sh tokenize test.lox"<<endl;
-        for(auto u : file_contents){
-            if(u == '{'){
-                cout<<"LEFT_BRACE { null"<<endl;
-            }
-            else if(u == '}'){
-                cout<<"RIGHT_BRACE } null"<<endl;
-            }
-            else if(u == '('){
-                cout<<"LEFT_PAREN ( null"<<endl;
-            }
-            else if(u == ')'){
-                cout<<"RIGHT_PAREN ) null"<<endl;
-            }
-            else if(u == '*'){
-                cout<<"STAR * null"<<endl;
-            }
-            else if(u == '.'){
-                cout<<"DOT . null"<<endl;
-            }
-            else if(u == '+'){
-                cout<<"PLUS + null"<<endl;
-            }
-            else if(u == ','){
-                cout<<"COMMA , null"<<endl;
-            }
-            else if(u == '-'){
-                cout<<"MINUS - null"<<endl;
-            }
-            else if(u == ';'){
-                cout<<"SEMICOLON ; null"<<endl;
-            }
-        }
-        std::cout << "EOF  null" << std::endl; // Placeholder, replace this line when implementing the scanner
-        
-    } else {
-        std::cerr << "Unknown command: " << command << std::endl;
-        return 1;
-    }
-
-    return 0;
+  return 0;
 }
 
-std::string read_file_contents(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error reading file: " << filename << std::endl;
-        std::exit(1);
-    }
+std::string read_file_contents(const std::string &filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "Error reading file: " << filename << std::endl;
+    std::exit(1);
+  }
 
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    file.close();
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  file.close();
 
-    return buffer.str();
+  return buffer.str();
 }
