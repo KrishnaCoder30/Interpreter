f88f9bd9b427a8f764820c6cb0526963a91a0131
Krishna Ishwar Deshmukh <krishna@Krishnas-MacBook-Air-805.local>
2026-03-22 18:45:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include <iostream>
 #include <map>
 #include <sstream>
 #include <string>
+
 using namespace std;
 
 std::string read_file_contents(const std::string &filename);
 
 int main(int argc, char *argv[]) {
-  // Disable output buffering
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // You can use print statements as follows for debugging, they'll be visible
-  // when running tests.
-  std::cerr << "Logs from your program will appear here!" << std::endl;
 
   if (argc < 3) {
     std::cerr << "Usage: ./your_program tokenize <filename>" << std::endl;
@@ -24,18 +19,12 @@ int main(int argc, char *argv[]) {
   }
 
   const std::string command = argv[1];
-//   map<char, string> token = {
-//       {'{', "LEFT_BRACE"},  {'}', "RIGHT_BRACE"}, {'(', "LEFT_PAREN"},
-//       {')', "RIGHT_PAREN"}, {'*', "STAR"},        {'.', "DOT"},
-//       {'+', "PLUS"},        {',', "COMMA"},       {'-', "MINUS"},
-//       {';', "SEMICOLON"},
-//   };
   if (command == "tokenize") {
     std::string file_contents = read_file_contents(argv[2]);
 
-    // TODO: Uncomment the code below to pass the first stage
-    //
-    cerr << "$ ./your_program.sh tokenize test.lox" << endl;
+    int line = 1;
+    bool has_error = false;
+
     for (auto u : file_contents) {
       if (u == '{') {
         cout << "LEFT_BRACE { null" << endl;
@@ -57,35 +46,30 @@ int main(int argc, char *argv[]) {
         cout << "MINUS - null" << endl;
       } else if (u == ';') {
         cout << "SEMICOLON ; null" << endl;
-      }
-      else if(u == '$'){
-        cout << "UNEXPECTED CHARACTER $ null" << endl;
-      }
-      else if(u == '#'){
-        cout << "UNEXPECTED CHARACTER # null" << endl;
+      } else if (u == ' ' || u == '\r' || u == '\t') {
+        // Ignore whitespace
+      } else if (u == '\n') {
+        ++line;
+      } else {
+        cerr << line << "] Error: Unexpected character: " << u << endl;
+        has_error = true;
       }
     }
-    std::cout << "EOF  null" << std::endl; // Placeholder, replace this line
-                                           // when implementing the scanner
 
+    if (has_error) {
+        return 65;
+    }
+
+    return 0;
   } else {
-    std::cerr << "Unknown command: " << command << std::endl;
+    cerr << "Usage: ./your_program tokenize <filename>" << endl;
     return 1;
   }
-
-  return 0;
 }
 
 std::string read_file_contents(const std::string &filename) {
-  std::ifstream file(filename);
-  if (!file.is_open()) {
-    std::cerr << "Error reading file: " << filename << std::endl;
-    std::exit(1);
-  }
-
-  std::stringstream buffer;
-  buffer << file.rdbuf();
-  file.close();
-
-  return buffer.str();
+  std::ifstream ifs(filename);
+  std::stringstream ss;
+  ss << ifs.rdbuf();
+  return ss.str();
 }
